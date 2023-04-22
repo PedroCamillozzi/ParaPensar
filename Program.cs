070ffbdb5f689d1f98b7d256da6e9f5c111672be
PedroCamillozzi <pedrocamillozzi01@gmail.com>
2023-04-22 00:14:49 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaPensarEj1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ingrese 2 numeros para su suma");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("El resultado de la suma de " + a.ToString() + " y " + b.ToString() + " es " + (a + b).ToString());
+            Console.ReadKey();
+        }
+    }
+}
